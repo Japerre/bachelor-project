49373bc6c3df1af46dbceae24458070969701dff
Japerre <tibolaperre@outlook.com>
2022-03-15 11:31:51 +0100

    eerste login succes met users

---
+++ backend/src/main/java/com/bachproject/demo/user/User.java
@@ -0,0 +1,29 @@
+package com.bachproject.demo.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private Long userId;
+    private String userName;
+    private String password;
+    private String role;
+
+}
