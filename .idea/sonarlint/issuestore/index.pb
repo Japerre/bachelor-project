3f02a8a44cad0bdd731d5c0c22ecec30fdf45d48
Elian Vantomme <elian.vantomme@hotmail.be>
2022-04-14 17:13:05 +0200

    Dutsende ...

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,5 +0,0 @@
-
-{
-Kbackend/src/main/java/com/bachproject/demo/promotor/PromotorController.java,f\6\f6f445a9ae61e73bf7c93e2169f0624e27449da8
-x
-Hbackend/src/main/java/com/bachproject/demo/promotor/PromotorService.java,8\9\8941282fd7e368fc7cc8c4e0bc3961d486069564
--- Frontend/FrontendV2/src/components/Authenticate/parseJWT.js
+++ Frontend/FrontendV2/src/components/Authenticate/parseJWT.js
@@ -1,11 +1,15 @@
 export default function parseJwt (token) {
     if(token) {
-        var base64Url = token.split('.')[1];
-        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var base64Url1 = token.split('.')[1];
+        var base64Url2 = token.split('.')[2];
+        var base64Url3 = token.split('.')[3];
+        var base64 = base64Url1.replace(/-/g, '+').replace(/_/g, '/');
         var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
         }).join(''));
 
-        return JSON.parse(jsonPayload);
+        console.log(base64)
+        let jsonData = JSON.parse(jsonPayload);
+        return jsonData;
     }
 }
