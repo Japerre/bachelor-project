882aa7108869f29f2f44daa6621b228064adc5ff
Japerre <tibolaperre@outlook.com>
2022-05-15 16:04:52 +0200

    bug fix

--- Frontend/FrontendV2/src/pages/viewer/HomeViewer.js
+++ Frontend/FrontendV2/src/pages/viewer/HomeViewer.js
@@ -5,7 +5,20 @@ import { Link } from "react-router-dom";
 
 const HomeViewer = () => {
   const [subjects, setSubjects] = useState([]);
+  const [subjectList, setSubjectList] = useState([]);
 
+  // authentication
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/whoami/user", {
+        headers: { authorization: localStorage.getItem("token") },
+      })
+      .then((data) => {
+        console.log(data.data);
+        setUser(data.data);
+      });
+  }, []);
   useEffect(() => {
     const getSubjects = async () => {
       const data = await axios.get(
@@ -15,33 +28,46 @@ const HomeViewer = () => {
         }
       );
       setSubjects(data.data);
+      setSubjectList(data.data);
     };
     getSubjects();
   }, []);
 
-  //authentication
-  const [user, setUser] = useState({});
-  useEffect(() => {
-    axios
-      .get("http://localhost:8080/whoami/user", {
-        headers: { authorization: localStorage.getItem("token") },
-      })
-      .then((data) => {
-        console.log(data.data)
-        setUser(data.data);
-      });
-  }, []);
+  const filterSubjects = (e) => {
+    const value = e.target.value.toLowerCase();
+    console.log(subjectList);
+    const filteredSubjects = subjectList.filter((subject) =>
+      `${subject.title} ${JSON.stringify(
+        subject.promotorList
+      )} ${JSON.stringify(subject.topicList)} ${JSON.stringify(
+        subject.targetAudienceList
+      )}} `
+        .toLowerCase()
+        .includes(value)
+    );
+    setSubjects(filteredSubjects);
+  };
 
   return (
     <>
       {!user.userId && (
-        <div>
-          <h1>you are currently not logged in</h1>
-          <Link to={"/login"}>
-            <button style={{ cursor: "pointer" }}>login</button>
-          </Link>
-        </div>
+        <center>
+          <div>
+            <h1>you are currently not logged in</h1>
+            <Link to={"/login"}>
+              <button style={{ cursor: "pointer" }}>login</button>
+            </Link>
+          </div>
+        </center>
       )}
+
+      <div className="search">
+        <input
+          className="searchbar"
+          placeholder="search on title, topics, promotors..."
+          onInput={filterSubjects}
+        />
+      </div>
 
       <div className="subject-container">
         <div className="grid-container">
